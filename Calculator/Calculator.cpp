1374ad705269a96995e014a21d9b7901c43a9812
nidip <nidip@ukr.net>
2025-01-31 13:06:26 +0200

    Начало Calculator.cpp

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    setlocale (LC_ALL, "ru");
+    cout << "Введите первый операнд" << endl;
+    return 0;
+}
