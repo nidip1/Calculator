ddd7590cd472ff18b1782ba099525a25c93b47d8
nidip <nidip@ukr.net>
2025-01-31 15:00:04 +0200

    Сделал цыкл do-while

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -9,25 +9,30 @@ int main()
     setlocale (LC_ALL, "ru");
     double a, b;
     char operation;
-    cout << "Введите первый операнд" << endl;
-    cin >> a;
-    cout << "Введите второй операнд" << endl;
-    cin >> b;
-    cout << "Введите операцию " <<'+' <<'\t' << '-'<<'\t' << '*' <<'\t' << '/' << endl;
-    cin >> operation;
-    Calc obj(a,b);
-    switch (operation)
-    {
-    case'+':cout << "Результат сложения " << obj.add(); break;
-    case'-':cout << "Результат вычитания " << obj.mines(); break;
-    case'*':cout << "Результат умножения " << obj.multiply(); break;
-    case'/':if (obj.divide() == 505.6)
-        cout << "На ноль делить нельзя"<<endl;
-           else
-        cout << "Результат деления " << obj.divide(); break;
-    default:
-        cout << "Вы ввели неправильную операцию" << endl;
+    char Answer;
+    do  {
+        cout << "Введите первый операнд" << endl;
+        cin >> a;
+        cout << "Введите второй операнд" << endl;
+        cin >> b;
+        cout << "Введите операцию " << '+' << '\t' << '-' << '\t' << '*' << '\t' << '/' << endl;
+        cin >> operation;
+        Calc obj(a, b);
+        switch (operation)
+        {
+        case'+':cout << "Результат сложения " << obj.add(); break;
+        case'-':cout << "Результат вычитания " << obj.mines(); break;
+        case'*':cout << "Результат умножения " << obj.multiply(); break;
+        case'/':if (obj.divide() == 505.6)
+            cout << "На ноль делить нельзя" << endl;
+               else
+            cout << "Результат деления " << obj.divide(); break;
+        default:
+            cout << "Вы ввели неправильную операцию" << endl;
+        }
+        cout << "\nХотите выйти из программы" << endl;
+        cin >> Answer;
     }
-
+    while (Answer != 'y');
     return 0;
 }
