399f737dc2ce8f4e143051a0f9c0d77f22646d74
nidip <nidip@ukr.net>
2025-01-31 14:20:33 +0200

    описал switch-case

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include "Calc.h"
 using std::cout;
 using std::cin;
 using std::endl;
@@ -14,6 +15,16 @@ int main()
     cin >> b;
     cout << "Введите операцию" <<'+' <<'\t' << '-'<<'\t' << '*' <<'\t' << '/' << endl;
     cin >> operation;
+    Calc obj(a,b);
+    switch (operation)
+    {
+    case'+':cout << "Результат сложения" << obj.add(); break;
+    case'-':cout << "Результат вычитания" << obj.mines(); break;
+    case'*':cout << "Результат умножения" << obj.multiply(); break;
+    case'/':cout << "Результат деления" << obj.divide(); break;
+    default:
+        
+    }
 
     return 0;
 }
